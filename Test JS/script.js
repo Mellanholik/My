3ae83ff41013337f12f90b018fef1cf0dba6d308
Mellanholik <ivanov.v.o.063@gmail.com>
2020-08-28 19:15:36 +0100

    hw20 add

---
+++ Test JS/script.js
@@ -0,0 +1,11 @@
+alert('Hello World!')
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+for (const key in object) {
+    if (object.hasOwnProperty(key)) {
+        const element = object[key];
+        
+    }
+}
